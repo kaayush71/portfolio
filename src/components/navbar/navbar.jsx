ee6972d587f3e5192df00a998fc4c854cb769ebc
Aayush Kumar <r3ptoid1@gmail.com>
2021-07-08 21:20:58 +0530

    fixed active class on links

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import styles from './navbar.module.scss';
 import NavbarIcon from '../navbarIcon/navbarIcon';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <section>
       {/* Desktop Navbar  */}
       <div className={styles.dcontainer}>
-        <NavbarIcon home text="home" path="/" />
-        <NavbarIcon user text="user" path="/about" />
-        <NavbarIcon portfolio text="portfolio" path="/work" />
-        <NavbarIcon contact text="contact" path="/contact" />
-        <NavbarIcon blog text="blogs" path="/blog" />
+        <NavLink
+          exact
+          to="/"
+          className={styles.link}
+          activeClassName={styles.active}
+        >
+          <NavbarIcon home text="home" path="/" />
+        </NavLink>
+        <NavLink
+          className={styles.link}
+          exact
+          to="/about"
+          activeClassName={styles.active}
+        >
+          <NavbarIcon user text="user" path="/about" />
+        </NavLink>
+        <NavLink
+          className={styles.link}
+          exact
+          to="/work"
+          activeClassName={styles.active}
+        >
+          <NavbarIcon portfolio text="portfolio" path="/work" />
+        </NavLink>
+
+        <NavLink
+          className={styles.link}
+          exact
+          to="/contact"
+          activeClassName={styles.active}
+        >
+          <NavbarIcon contact text="contact" path="/contact" />
+        </NavLink>
+        <NavLink
+          className={styles.link}
+          exact
+          to="/blog"
+          activeClassName={styles.active}
+        >
+          <NavbarIcon blog text="blogs" path="/blog" />
+        </NavLink>
       </div>
     </section>
   );
--- src/components/navbarIcon/navbarIcon.jsx
+++ src/components/navbarIcon/navbarIcon.jsx
@@ -1,39 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './navbarIcon.module.scss';
-import { Link } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
 import { FaUser } from 'react-icons/fa';
 import { FaBriefcase } from 'react-icons/fa';
 import { FaEnvelope } from 'react-icons/fa';
 import { FaComment } from 'react-icons/fa';
 
-const Navbar = ({ home, user, portfolio, contact, blog, text, path }) => {
-  const [active, setActive] = useState();
-
-  function handleClick() {
-    // home = false;
-    setActive(true);
-  }
+const Navbar = ({ home, user, portfolio, contact, blog, text }) => {
   return (
-    <div
-      onClick={() => handleClick()}
-      className={
-        active
-          ? styles.iconbox_active
-          : // : home
-            // ? styles.iconbox_active
-            styles.iconbox
-      }
-    >
+    <div className={styles.iconbox}>
       {home && <FaHome className={styles.iconbox_icon} />}
       {user && <FaUser className={styles.iconbox_icon} />}
       {portfolio && <FaBriefcase className={styles.iconbox_icon} />}
       {contact && <FaEnvelope className={styles.iconbox_icon} />}
       {blog && <FaComment className={styles.iconbox_icon} />}
 
-      <Link to={path} className={styles.iconbox_link}>
+      <div className={styles.iconbox_link}>
         <h2 className={styles.iconbox_link__text}>{text}</h2>
-      </Link>
+      </div>
     </div>
   );
 };
