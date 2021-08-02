fda6a1e9f67e3c2741beb72ed44343dba44a708e
Aayush Kumar <r3ptoid1@gmail.com>
2021-08-03 01:50:58 +0530

    updated contact page

--- src/pages/contactPage/ContactPage.jsx
+++ src/pages/contactPage/ContactPage.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import styles from './contactPage.module.scss';
+import { FaEnvelopeOpen } from 'react-icons/fa';
+import { FaPhoneSquare } from 'react-icons/fa';
+import { SiGithub } from 'react-icons/si';
+import { SiTwitter } from 'react-icons/si';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+
 
 export default function ContactPage() {
   return (
@@ -9,6 +16,41 @@ export default function ContactPage() {
           GET IN <span className={styles.heading_sub}>TOUCH</span>
         </h1>
         <span className={styles.sub}>CONTACT</span>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.main_left}>
+          <h2 className={styles.main_left__head}>DON'T BE SHY !</h2>
+          <p className={styles.main_left__para}>
+            Feel free to get in touch with me. I am always open to discussing
+            new projects, creative ideas or opportunities to be part of your
+            visions.
+          </p>
+          <div className={styles.main_left__con}>
+            <FaEnvelopeOpen className={styles.conicons} />
+            <p>kaayush.ak71@gmail.com</p>
+          </div>
+          <div className={styles.main_left__con}>
+            <FaPhoneSquare className={styles.conicons} />
+            <p>9870275606</p>
+          </div>
+          <div className={styles.main_left__social}>
+            <a>
+              <SiGithub />
+            </a>
+            <a>
+              <SiTwitter />
+            </a>
+            <a>
+              <FaLinkedinIn />
+            </a>
+            <a>
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+        <div className={styles.main_right}>
+          <h1>Hello</h1>
+        </div>
       </div>
     </div>
   );
