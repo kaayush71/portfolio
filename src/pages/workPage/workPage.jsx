56897d2c7c8692e6e91bb0e6bae6080db6d2bea0
Aayush Kumar <r3ptoid1@gmail.com>
2021-10-15 23:18:13 +0530

    updated portfolio page

--- src/pages/workPage/workPage.jsx
+++ src/pages/workPage/workPage.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import styles from './workPage.module.scss';
+import todo from '../../assets/todo.png';
+import venrate from '../../assets/venrate.png';
+import weather from '../../assets/weather.png';
+import url from '../../assets/url.png';
+import portfolio from '../../assets/portfolio.png';
+import hastebin from '../../assets/hastebin.png';
+import currency from '../../assets/currency.png';
 
 export default function WorkPage() {
   return (
@@ -12,10 +19,60 @@ export default function WorkPage() {
           <span className={styles.sub}>Works</span>
         </div>
         <div className={styles.grid}>
-          <div className={styles.card}>A</div>
-          <div className={styles.card}>A</div>
-          <div className={styles.card}>A</div>
-          <div className={styles.card}>A</div>
+          <div data-aos="fade-right" className={styles.card}>
+            <img className={styles.card_img} src={venrate} alt="todo" />
+            <h1 className={styles.card_title}>Venrate</h1>
+            <div className={styles.card_sub}>
+              <p>Nuxt Js</p>
+              <p>Ceramic Blockchain</p>
+            </div>
+          </div>
+          <div data-aos="fade-left" className={styles.card}>
+            <img className={styles.card_img} src={todo} alt="todo" />
+            <h1 className={styles.card_title}>Todo App</h1>
+            <div className={styles.card_sub}>
+              <p>React Js</p>
+              <p>Firebase Firestore</p>
+            </div>
+          </div>
+          <div data-aos="fade-right" className={styles.card}>
+            <img className={styles.card_img} src={weather} alt="weather" />
+            <h1 className={styles.card_title}>Weather App</h1>
+            <div className={styles.card_sub}>
+              <p>React Js</p>
+            </div>
+          </div>
+          <div data-aos="fade-left" className={styles.card}>
+            <img className={styles.card_img} src={url} alt="weather" />
+            <h1 className={styles.card_title}>Url Shortner</h1>
+            <div className={styles.card_sub}>
+              <p>React Js</p>
+              <p>Express Js</p>
+              <p>Mongodb Atlas</p>
+            </div>
+          </div>
+          <div data-aos="fade-right" className={styles.card}>
+            <img className={styles.card_img} src={portfolio} alt="weather" />
+            <h1 className={styles.card_title}>Portfolio</h1>
+            <div className={styles.card_sub}>
+              <p>React Js</p>
+            </div>
+          </div>
+          <div data-aos="fade-left" className={styles.card}>
+            <img className={styles.card_img} src={hastebin} alt="weather" />
+            <h1 className={styles.card_title}>Hastebin Clone</h1>
+            <div className={styles.card_sub}>
+              <p>React Js</p>
+              <p>Mongodb</p>
+            </div>
+          </div>
+          <div data-aos="fade-right" className={styles.card}>
+            <img className={styles.card_img} src={currency} alt="weather" />
+            <h1 className={styles.card_title}>Currency Converter</h1>
+            <div className={styles.card_sub}>
+              <p>React Js</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
